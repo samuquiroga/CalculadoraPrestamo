80b77c5500c38f53b79c751ec4aa698dae11b8bd
samuquiroga <113557597+samuquiroga@users.noreply.github.com>
2023-06-04 18:43:51 -0300

    Cambios4

--- CalculadoraPrestamo/Controllers/CreditoController.cs
+++ CalculadoraPrestamo/Controllers/CreditoController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalculadoraPrestamo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CalculadoraPrestamo.Controllers
 {
     public class CreditoController : Controller
     {
-        public IActionResult AbmCredito()
+        private readonly CalculadoraPrestamoWeb _context;
+
+        public CreditoController(CalculadoraPrestamoWeb context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> AbmCredito()
+        {
+            return View(await _context.Credito.ToListAsync());
+        }
+
+
+
+
     }
 }
