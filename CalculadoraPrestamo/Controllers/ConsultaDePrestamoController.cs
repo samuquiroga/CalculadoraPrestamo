a01126db12325efc2f136a0170bd591efa4e6a6a
samuquiroga <113557597+samuquiroga@users.noreply.github.com>
2023-06-11 19:18:37 -0300

    cambios6

--- CalculadoraPrestamo/Controllers/ConsultaDePrestamoController.cs
+++ CalculadoraPrestamo/Controllers/ConsultaDePrestamoController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalculadoraPrestamo.Data;
+using CalculadoraPrestamo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CalculadoraPrestamo.Controllers
 {
     public class ConsultaDePrestamoController : Controller
     {
-        public IActionResult AbmConsulta()
+
+
+        private readonly CalculadoraPrestamoWeb _context;
+
+        public ConsultaDePrestamoController(CalculadoraPrestamoWeb context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarPrestamo(ConsultaPrestamo prestamo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(prestamo);
+                await _context.SaveChangesAsync();
+                TempData["Mensaje"] = "Prestamo creado satisfactoriamente";
+                return RedirectToAction("AbmConsulta", "ConsultaDePrestamo");
+            }
+            return View(prestamo);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AbmConsulta()
+        {
+            return View(await _context.ConsultaPrestamo.ToListAsync());
+        }
+
+       
     }
 }
--- CalculadoraPrestamo/Controllers/InteresController.cs
+++ CalculadoraPrestamo/Controllers/InteresController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalculadoraPrestamo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CalculadoraPrestamo.Controllers
 {
     public class InteresController : Controller
     {
-        public IActionResult AbmInteres ()
+        private readonly CalculadoraPrestamoWeb _context;
+
+        public InteresController(CalculadoraPrestamoWeb context)
         {
-            return View();
+            _context = context;
         }
+        
+        [HttpGet]
+        public async Task<IActionResult> AbmInteres()
+        {
+            var intereses = _context.Interes.Include(i => i.Credito).ToList();
+            return View(await _context.Interes.ToListAsync());
+        }
+
     }
 }
